4492d3a4abafdd97b7532011058eb10b8837aa53
eda <edanurmert1227@gmail.com>
2025-12-27 15:36:14 +0300

    Add files via upload
    Çözüm ve SQL dosyaları eklendi

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿-- Önce hatalı tabloyu siliyoruz
+DROP TABLE TransferTbl;
+
+-- Şimdi doğrusunu oluşturuyoruz
+CREATE TABLE TransferTbl (
+    TNum INT PRIMARY KEY IDENTITY(1,1),
+    HAdSoyad NVARCHAR(50),
+    KGrup NVARCHAR(10)
+);
